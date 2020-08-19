1826aff08b7f87feed29494214525904173ccbc9
Stef van Esch <stef@marshmallow.dev>
2020-08-19 17:28:45 +0200

    Styling update for index

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -2,22 +2,17 @@
     <div v-if="field.value" class="py-3 tags-index-field">
         <div v-if="field.value.pagespeed_score >= field.perfect.pagespeed_score && field.value.yslow_score >= field.perfect.yslow_score">
             <span class="whitespace-no-wrap px-2 py-1 rounded-full uppercase text-xs font-bold bg-success-light text-success-dark underline-none">
-                Perfect score :)
+                :)
             </span>
         </div>
         <div v-else>
             <a v-bind:href="field.value.report_url" target="_blank" class="underline-none">
                 <div class="whitespace-no-wrap px-2 py-1 rounded-full uppercase text-xs font-bold metrix-badge"
-                      v-bind:class="badgeColor('pagespeed_score')">
-                    Pagespeed: {{field.value.pagespeed_score}}
-                </div>
-                <div class="whitespace-no-wrap px-2 py-1 rounded-full uppercase text-xs font-bold metrix-badge"
-                      v-bind:class="badgeColor('yslow_score')">
-                    Yslow: {{field.value.yslow_score}}
+                      v-bind:class="badgeColor('average')">
+                    Avg: {{average()}}
                 </div>
             </a>
         </div>
-
     </div>
     <div v-else>
         <span class="whitespace-no-wrap px-2 py-1 rounded-full uppercase text-xs font-bold">
@@ -32,9 +27,22 @@ export default {
     props: ['field'],
 
     methods: {
+
+        average() {
+            return Math.floor((+this.field.value.pagespeed_score + +this.field.value.yslow_score) / 2)
+        },
+
         badgeColor(score_name) {
+            if (score_name == 'average') {
+                let average = this.average()
+                return this.colorByValue(average)
+            }
 
             let value = this.field.value[score_name]
+            return this.colorByValue(value)
+        },
+
+        colorByValue(value) {
             let badges = this.field.badges
 
             if (value >= badges.success) {
