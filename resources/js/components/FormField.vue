61728ea145d011b029b4e37c60db68d78b088415
LTKort <lars.kort94@gmail.com>
2022-05-04 18:01:30 +0200

    fix js

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,3 +1,10 @@
 <template>
+    <DefaultField
+        :field="field"
+        :errors="errors"
+        :show-help-text="showHelpText"
+    >
+        <template #field> </template>
+    </DefaultField>
     <!-- No FormField type available -->
 </template>
